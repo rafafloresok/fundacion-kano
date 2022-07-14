f6a1e6d2800f231a34f2d3542ca23de5485095dc
rafafloresok <rafafloresok@gmail.com>
2022-07-14 19:42:28 -0300

    se optimiza codigo

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+/* LANDING CAROUSEL */
 const landingImgSrc = [
     "../images/landing/landing_01.jpg",
     "../images/landing/landing_02.jpg",
@@ -17,3 +18,10 @@ setInterval(() => {
     }, 1500);
     i === (landingImgSrc.length-1) ? i = 0 : i++;
 }, 3500);
+
+/* NAVBAR TOGGLE */
+const navToggleBtn = document.querySelector('#nav-toggle')
+const navBar = document.querySelector('#navbar')
+const navBarItems = document.querySelectorAll('.navbar__item')
+navToggleBtn.addEventListener('click', () => navBar.classList.toggle('navbar--visible'))
+navBarItems.forEach(item => item.addEventListener('click', () => navBar.classList.toggle('navbar--visible')))
